70ed53eab50b0d95ebb85da96f84dba8a3d18e6f
Sem Salimov <salimovsemen43@gmail.com>
2023-09-05 22:31:38 +0600

    6. Условные операторы
    *Решены: с 0 по 7

--- tasks/task_6.py
+++ tasks/task_6.py
@@ -1,1 +1,11 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+year = int(input("Введите год: "))
+manth = int(input("Введите месяц: ")) 
+
+leap_year: bool = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+unic_manth = [1, 3, 5, 7, 8, 10, 12]
+
+if leap_year and manth in unic_manth:
+    print("Да")
+else:
+    print("Нет")
+    
--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,1 +1,10 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+name = int(input(""))
+error = float(input(""))
+unic = str(input(""))
+
+search = dict( name = int(), error = float, unic = str)
+
+if search is True:
+    print("Да")
+else:
+    print("Нет")
