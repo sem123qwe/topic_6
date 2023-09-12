da3d3c55cc9b005c0c78ab1d02c398263d10cb1e
Sem Salimov <salimovsemen43@gmail.com>
2023-09-12 21:21:15 +0600

    6. Условные операторы  * Исправленны номер: 8, 10

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -10,22 +10,27 @@ print("Добро пожаловать в программу \"Буква-Дет
 print("Выберите алфавит: \n""1. Латинский\n""2. Кириллица\n")
 
 choice = int(input("Введите номер алфавита: "))
-if choice == 1:
-    print("Латинский")
-elif choice == 2:
-    print("Кириллица")
+
+if choice !=1 and choice !=2:
+    print("Упс! Выбран неверный режим. Попробуйте ещё раз...")
+elif choice == 1:
+    language = "Введите букву латинского алфавита: "
+    print(end= "\r")
 else:
-    print(" Упс! Выбран неверный режим. Попробуйте ещё раз...")
+    language = "Введите букву кириллицы: "
+    print(end= "\r")
 
-choice_abc = input("Введите букву латинского алфавита: ")
+choice_abc = input(language)
 
-if choice_abc == ALPHABETS["en_vowels"]:
+if choice_abc in ALPHABETS["en_vowels"]:
     print(ALPHABETS["en_vowels"],"- гласная буква")
-elif choice_abc == ALPHABETS["en_consonants"]:
+elif choice_abc in ALPHABETS["en_consonants"]:
     print(ALPHABETS,"- согласная буква")
-elif choice_abc == ALPHABETS["ru_vowels"]:
+
+
+elif choice_abc in ALPHABETS["ru_vowels"]:
     print(ALPHABETS["ru_vowels"],"- гласная буква")
-elif choice_abc == ALPHABETS["ru_consonants"]:
-    print(ALPHABETS["ru_consonants"],"- согласная буква")
+elif choice_abc in ALPHABETS["ru_consonants"]:
+     print(ALPHABETS["ru_consonants"],"- согласная буква")
 else:
     print("Упс! Неизвестная буква. Попробуйте другую!")
--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,21 +1,16 @@
 num_1: int = int(input("Введите первое число: "))
 num_2: int = int(input("Введите второе число: "))
 
-spell_1: int = num_1 + num_2
-spell_2: int = -(-num_1 + num_2)
-spell_3: int = num_1 * num_2
-spell_4: bool = num_1 == 0 + num_2 != 0
-spell_5: bool = num_1 == 0 + num_2 == 0
-
 magic: str = input("Введите магическую операцию: ")
 
 if magic == "Призыв":
-    print("Сумма магических сил чисел:", spell_1)
+    print("Сумма магических сил чисел:", num_1 + num_2)
 elif magic == "Трансформация":
-    print("Трансформированное число:", spell_2)
+    print("Трансформированное число:", num_1 - num_2)
 elif magic == "Объединение":
-    print("Объединение чисел:", spell_3)
+    print("Объединение чисел:", num_1 * num_2)
 elif magic == "Исчезновение":
-    print("Исчезновение:", spell_4 or "Ошибка: Второе число равно нулю!")
+    print("Исчезновение:", num_1 / num_2 
+          or "Ошибка: Второе число равно нулю!")
 else:
     print("Ошибка: Некорректная операция")
